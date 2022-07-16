3d29b97a5baa5c2bdc623a707d5f4b06bb210747
Igor Cebota <chebota2@gmail.com>
2022-07-16 20:31:53 +0300

    2 пункт

--- Lesson-3/Program.cs
+++ Lesson-3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Write("Введите строку: ");
+Console.WriteLine(ReverseString(Console.ReadLine()));
+
+Console.ReadLine();
+
+static string ReverseString(string primary) 
+{
+    char[] chars = primary.ToCharArray();
+    char[] reverse = new char[chars.Length];
+    string final = string.Empty;
+    for (int i = chars.Length-1; i >= 0; i--)
+    {
+        int a = 0;
+        reverse[a] = chars[i];
+        final+=reverse[a];
+        a++;
+    }
+    return final;
+}
