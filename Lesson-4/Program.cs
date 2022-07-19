9348972e67c1778e87dc2f35db7ce20c69f13af2
Igor Cebota <chebota2@gmail.com>
2022-07-19 15:57:22 +0300

    1 пункт

---
+++ Lesson-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lesson_4;
+
+var build1 = new Building();
+Console.WriteLine(build1);
+var build2 = new Building(10,4);
+Console.WriteLine(build2);
+var build3 = new Building(9,2,4);
+Console.WriteLine(build3);
+var build4 = new Building(12, 5, 3, 3);
+Console.WriteLine(build4);
+var build5 = new Building(20, 8, 1, 2);
+Console.WriteLine(build5);
+
+Console.WriteLine("Завершение программы...");
+Console.ReadLine();
